505f6c6f70386cd6fde2b5b81bda955f1e2f226c
Krishna Konda <krishnankonda@gmail.com>
2023-11-26 17:13:21 -0800

    Implemented Home Page

---
+++ src/client/src/App.js
@@ -0,0 +1,66 @@
+// App.js
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import './App.css';
+import GameDetails from './GameDetails';
+
+
+function App() {
+  const [backendData, setBackendData] = useState([]);
+
+  useEffect(() => {
+    fetch("/games")
+      .then((res) => res.json())
+      .then((data) => {
+        setBackendData(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  const truncateDescription = (description, maxLength) => {
+    if (description.length > maxLength) {
+      return `${description.slice(0, maxLength)}...`;
+    }
+    return description;
+  };
+
+  return (
+    <Router>
+      <div>
+        <header className="app-header">
+          <h1>SteamEngine</h1>
+        </header>
+        <div className="game-container">
+          {backendData.map((game) => (
+            <div key={game.GameID} className="game-card">
+              <img src={game.HeaderImageURL} alt={game.GameName} />
+              <div className="game-details">
+                <h2>{game.GameName}</h2>
+                <p>{truncateDescription(game.Description, 220)}</p>
+                {(
+                  <Link to={`/games/${game.GameID}`} className="show-more-button">
+                    Show more
+                  </Link>
+                )}
+                <p>Release Date: {new Date(game.ReleaseDate).toLocaleDateString()}</p>
+                <p>Price: ${game.Price}</p>
+                <p>MetaCritic Score: {game.MetaCritic}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <Routes>
+          <Route
+            path="/games/:id"
+            element={<GameDetails backendData={backendData} />}
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+
+export default App;
